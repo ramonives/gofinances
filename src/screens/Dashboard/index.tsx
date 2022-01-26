ec7b6c91f034917c291025449311ace9711c7f67
Ramon Ives <ramonivesdev@gmail.com>
2022-01-26 08:33:19 -0300

    Criando estilos globais

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Text } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  );
+}
