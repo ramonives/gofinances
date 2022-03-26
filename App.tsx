599b6a79374764af74565aba75c95fb456736306
Ramon Ives <ramonivesdev@gmail.com>
2022-03-26 17:55:36 -0300

    tab navigation

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import AppLoading from "expo-app-loading";
+import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "styled-components";
+import AppLoading from "expo-app-loading";
 
+import { AppRoutes } from "./src/routes/app.routes";
+
+import theme from "./src/global/styles/theme";
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
-
-import theme from "./src/global/styles/theme";
-
-import { Register } from "./src/screens/Register";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,10 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <StatusBar barStyle={"light-content"} />
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
